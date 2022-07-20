8aebb25300b25cb5b28b5e584e1d02fdaccb8257
EduardoRmrz <106790128+EduardoRmrz@users.noreply.github.com>
2022-07-19 19:03:13 -0500

    Hicimos la primera vista de html libros

--- EdbooksApp/views.py
+++ EdbooksApp/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, Template
 
+from EdbooksApp.models import Libro
+
 # Create your views here.
 def inicio(request):
     hoy = datetime.datetime.now()
     return render(request, "index.html", {"dia_hora":hoy})
 
 def libros(request):
-    return HttpResponse("Vista de libros")
+    # return HttpResponse("Vista de libros")
+    libros = Libro.objects.all()
+    return render(request, "libros.html", {"libros":libros})
 
 def usuarios(request):
     return HttpResponse("Vista de usuarios")
